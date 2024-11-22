d7b09ac82308e7ef2711a7af3387ead621f6582c
YASHWANTHGOWDAKS5 <yashwanthgowdaksyashugowda@gmail.com>
2024-11-22 19:36:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # Serve the updated HTML file
+    return render_template('hello.html')  # Serve the updated HTML file
 
 if __name__ == '__main__':
     app.run(debug=True)
